32126b61e26b2455595d10dac10641691b9417a1
Dmitriy Chechel <Betelgeuse1920@gmail.com>
2017-03-30 22:35:07 +0300

    Mysql settings

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=localhost;port=5432;dbname=delivery',
-    'username' => 'postgres',
-    'password' => '123123',
+    'dsn' => 'mysql:host=gi6kn64hu98hy0b6.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;port=3306;dbname=g04bns9q8pfqs5o8',
+    'username' => 'n2qsgnfih1o8kxbf',
+    'password' => 'bqhipb4d5cr38uvo',
     'charset' => 'utf8',
 ];
