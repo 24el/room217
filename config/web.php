1895a9017f0ec9ce6cf1d796a7eb2fbd145eadb3
dmChechel <Betelgeuse1920@gmail.com>
2017-05-24 00:37:39 +0300

    Update web.php

--- config/web.php
+++ config/web.php
@@ -37,6 +37,7 @@ $config = [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                '<action>' => 'site/<action>',
             ],
         ],
         
